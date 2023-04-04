e664bc66f00662ab1085714a98a82795f9445230
Luke Chen <heylukechen@gmail.com>
2023-04-04 18:19:12 +0200

    seventh iteration

--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -1,29 +1,49 @@
-const queueUL = document.querySelector('.list-queue');
-const queueInput = document.querySelector('.queue-input');
-const warningTopQueue = document.querySelector('#queue-container .warning-top');
+const queueUL = document.querySelector(".list-queue");
+const queueInput = document.querySelector(".queue-input");
+const warningTopQueue = document.querySelector("#queue-container .warning-top");
 const warningBottomQueue = document.querySelector(
-  '#queue-container .warning-bottom'
+  "#queue-container .warning-bottom"
 );
-const addQueue = document.querySelector('.btn-add-queue');
-const dequeue = document.querySelector('.btn-take-dequeue');
+const addQueue = document.querySelector(".btn-add-queue");
+const dequeue = document.querySelector(".btn-take-dequeue");
 
 const queue = new Queue();
 
 const clearQueueInput = () => {
-  // ... your code goes here
+  queueInput.value = "";
 };
 
 const generateListQueue = () => {
-  // ... your code goes here
+  warningTopQueue.style.display = "none";
+  warningBottomQueue.style.display = "none";
+  queueUL.innerHTML = "";
+  let length = queue.display().length;
+  let size = queue.MAX_SIZE - length;
+  queue.display().forEach((item) => {
+    let li = document.createElement("li");
+    li.className = 'active';
+    li.innerText = item;
+    queueUL.appendChild(li);
+  });
+
+  for (let i = 0; i < size; i++){
+    let li = document.createElement('li');
+    li.classList = 'inactive';
+    li.innerHTML = '&nbsp;';
+    queueUL.appendChild(li);
+  }
 };
 
 generateListQueue();
 
 const generateWarningQueue = (type) => {
-  if (type === 'underflow') {
+  if (type === "underflow") {
+  warningBottomQueue.style.display = 'block';
+  warningBottomQueue.innerText = type;
+  } else if (type === "overflow") {
     // ... your code goes here
-  } else if (type === 'overflow') {
-    // ... your code goes here
+    warningTopQueue.style.display = 'block';
+    warningTopQueue.innerText = type;
   }
 };
 
@@ -43,5 +63,5 @@ const removeFromQueue = () => {
   }
 };
 
-addQueue.addEventListener('click', addToQueue);
-dequeue.addEventListener('click', removeFromQueue);
+addQueue.addEventListener("click", addToQueue);
+dequeue.addEventListener("click", removeFromQueue);
